18a6b89e8d28051cde8dc1baa8e6b4844aa4360a
Jay3104 <jna3104@outlook.com>
2024-03-07 15:18:33 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,16 +8,21 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
+        Private int _Face random = new Random();
         /*
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
 
         //Property
-
+        Public int Value {Get; private set; }
 
         //Method
-
+        Public int Roll()
+        {
+            Value = random.Next(1,7);
+            return value;
+        }
 
     }
 }
